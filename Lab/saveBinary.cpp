d9fc484487749fc301964a1706a6ec9e0ebf2d4b
Yanqing Lu <bubu_lu@126.com>
2020-10-11 02:04:17 +0800

    v0.1.3

---
+++ Lab/saveBinary.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <random>
+#include <vector>
+using namespace std;
+
+float generateRand(){ //生成随机数组
+    float a1;
+    random_device r1;
+    a1 = r1() / float(RAND_MAX / 100);
+    return a1;
+}
+
+
+int main()
+{
+   FILE * fp = NULL;
+   
+   fp = fopen("/Users/leolu/Downloads/C:C++/Assignments/cmake-build-debug/data1", "wb"); // 以二进制写入的形式打开
+
+   float *ran;
+   ran = new float[200000000]; // 动态分配数组
+   for (int i =0 ; i < 200000000; ++i)
+   {
+      ran[i] = generateRand();
+   }
+
+   fwrite(ran,sizeof(float), 200000000, fp); //二进制写
+
+   delete []ran;
+   fclose(fp);
+   return 0; 
+}
