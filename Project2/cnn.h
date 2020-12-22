96bc8a952c3fb61f48ac9d918a76f5e86ddf71de
Yanqing Lu <bubu_lu@126.com>
2020-12-23 01:55:55 +0800

    version 1.0 --no optimization

--- Project2/cnn.h
+++ Project2/cnn.h
@@ -7,25 +7,48 @@
 
 #include <iostream>
 #include "opencv2/opencv.hpp"
-#include "face_binary_cls.h"
 using namespace cv;
 using namespace std;
 
 class Image {
 private:
-    int rows;
-    int cols;
-    int channels;
+    int height;
+    int width;
+    int depth;
     int length;
     float *data;
 public:
-    Image(int rows, int cols, int channels, float *data);
-    void conv(conv_param & convParam);
-    void maxPool();
+    Image(int rows, int cols, int channels, float * data);
+    ~Image();
+
+    void conv(int out_dep, int in_dep, int ker_size, int pad, int stride,
+             float * weight, const float * bias);
     void reLU();
-    void fc(fc_param & fcParam);
+    void max_pool();
+    void fc(int out_dep, float * weight, const float * bias);
+
+    void show();
 };
 
-void * convertToFloat(const Mat & image, float * data);
+template<int depth>
+class Layers {
+private:
+    typedef float array[depth]; //将深度为depth的float数组定义为类型array，方便运算
+    array ** data;
+    int rows;
+    int cols;
+public:
+    Layers(int rows, int cols);
+    ~Layers();
+    void init(const Mat & image);
+    void conv(int out_dep, int in_dep, int ker_size, int pad, int stride,
+              const float * weight, const float * bias, Layers & layers);
+    void reLU();
+    void max_pool(Layers & layers);
+    void flatten(float * out);
+};
+
+void convert2float(const Mat & image, float * data);
+float max_4(float f1, float f2, float f3, float f4); //4个浮点数取最大值
 
 #endif //PROJECT2_CNN_H
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,17 +1,44 @@
 #include "cnn.h"
+#include "face_binary_cls.cpp"
 
 int main() {
     Mat origin;
-    origin = imread("/Users/leolu/Downloads/C++/SimpleCNNbyCPP/samples/face.jpg");
-    cout << origin.total() << endl;
-    cout << origin.channels() << endl;
-
+    origin = imread("/Users/leolu/Downloads/C++/SimpleCNNbyCPP/samples/bg.jpg");
+    cout << origin.channels() << "*" << origin.rows << "*" << origin.cols << endl;
     auto * data = new float[origin.total()*3];
-    convertToFloat(origin,data);
-    Image layer0(origin.rows,origin.cols,origin.channels(),data);
+    convert2float(origin,data);
+    Image image(origin.rows,origin.cols,origin.channels(),data);
 
-    delete [] data;
-//    waitKey(0);
+//3次卷积操作
+    for (int i = 0; i < 3; ++i) {
+        image.conv(conv_params[i].out_channels,conv_params[i].in_channels,conv_params[i].kernel_size,
+                   conv_params[i].pad,conv_params[i].stride,conv0_weight,conv0_bias);
+//        cout << "conv " << i << endl;
+//        image.show();
+
+        image.reLU();
+//        cout << "reLU " << i << endl;
+//        image.show();
+
+        image.max_pool();
+//        cout << "max " << i << endl;
+//        image.show();
+    }
+//全连接
+    image.fc(fc_params->out_features,fc0_weight,fc0_bias);
+//展示结果
+    image.show();
+//    Layers<3> layers0(origin.rows,origin.cols);
+//    layers0.init(origin);
+//
+//    Layers<16> layers1(origin.rows/2,origin.cols/2);
+//    Layers<32> layers2(origin.rows/4,origin.cols/4);
+//    Layers<32> layers3(origin.rows/8,origin.cols/8);
+//    for (int i = 0; i < origin.rows; ++i)
+//        delete [] data[i];
+//    delete [] data;
+
     return 0;
 }
+
 
