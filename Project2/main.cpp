3f0ca3fcbfdf00deaa2caead9a89697b4e44b19e
Yanqing Lu <bubu_lu@126.com>
2020-12-25 15:41:49 +0800

    o3 and code logic

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,43 +1,59 @@
 #include "cnn.h"
 #include "face_binary_cls.cpp"
+#include <chrono>
 
 int main() {
     Mat origin;
-    origin = imread("/Users/leolu/Downloads/C++/SimpleCNNbyCPP/samples/bg.jpg");
-    cout << origin.channels() << "*" << origin.rows << "*" << origin.cols << endl;
-    auto * data = new float[origin.total()*3];
-    convert2float(origin,data);
-    Image image(origin.rows,origin.cols,origin.channels(),data);
+    origin = imread("/Users/leolu/Downloads/C++/Project2_1/image/bg4.jpeg");
+
+    for (int i = 0; i < 10; ++i) {
+
+        cout << "bg4(double), ";
+//        cout << "channels*rows*cols = " << origin.channels() << "*" << origin.rows << "*" << origin.cols << endl;
+        auto *data = new double[origin.total() * 3];
+
+        auto start = std::chrono::steady_clock::now();
+
+        convert2float(origin, data);
+        Image image(origin.rows, origin.cols, origin.channels(), data);
 
 //3次卷积操作
-    for (int i = 0; i < 3; ++i) {
-        image.conv(conv_params[i].out_channels,conv_params[i].in_channels,conv_params[i].kernel_size,
-                   conv_params[i].pad,conv_params[i].stride,conv0_weight,conv0_bias);
+        for (auto &conv_param : conv_params) {
+//            auto inter0 = std::chrono::steady_clock::now();
+            image.conv(conv_param.out_channels, conv_param.in_channels, conv_param.kernel_size,
+                       conv_param.pad, conv_param.stride, conv_param.p_weight, conv_param.p_bias);
 //        cout << "conv " << i << endl;
 //        image.show();
+//            auto inter1 = std::chrono::steady_clock::now();
+//            auto dur0 = std::chrono::duration_cast<std::chrono::milliseconds>(inter1 - inter0).count();
+//            cout << "convolution costs " << dur0 << " ms, ";
 
-        image.reLU();
+            image.reLU();
 //        cout << "reLU " << i << endl;
 //        image.show();
+//            auto inter2 = std::chrono::steady_clock::now();
+//            auto dur1 = std::chrono::duration_cast<std::chrono::milliseconds>(inter2 - inter1).count();
+//            cout << "reLU costs " << dur1 << " ms, ";
 
-        image.max_pool();
+            image.max_pool();
 //        cout << "max " << i << endl;
 //        image.show();
-    }
+//            auto inter3 = std::chrono::steady_clock::now();
+//            auto dur2 = std::chrono::duration_cast<std::chrono::milliseconds>(inter3 - inter2).count();
+//            cout << "max pooling costs " << dur2 << " ms, ";
+        }
 //全连接
-    image.fc(fc_params->out_features,fc0_weight,fc0_bias);
-//展示结果
-    image.show();
-//    Layers<3> layers0(origin.rows,origin.cols);
-//    layers0.init(origin);
-//
-//    Layers<16> layers1(origin.rows/2,origin.cols/2);
-//    Layers<32> layers2(origin.rows/4,origin.cols/4);
-//    Layers<32> layers3(origin.rows/8,origin.cols/8);
-//    for (int i = 0; i < origin.rows; ++i)
-//        delete [] data[i];
-//    delete [] data;
+        image.fc(fc_params->out_features, fc_params->p_weight, fc_params->p_bias);
 
+        auto end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        cout << "duration = " << duration << "ms, ";
+
+//展示结果
+        image.show();
+        cout << endl;
+    }
     return 0;
 }
 
