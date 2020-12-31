3962e6b5f5d407e0d3b6922103d2d62c8f9d3847
Yanqing Lu <bubu_lu@126.com>
2020-12-31 15:03:11 +0800

    add a test file

--- Project2/test_conv.cpp
+++ Project2/test_conv.cpp
@@ -7,10 +7,10 @@ int main() {
     origin = imread("../image/face.jpg");
 //    cout << "channels*rows*cols = " << origin.channels() << "*" << origin.rows << "*" << origin.cols << endl;
 
-    cout << "(face & conv) * 100, ";
-    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10; ++i) {
+        cout << "face & o3, ";
+        auto start = std::chrono::steady_clock::now();
 
-    for (int i = 0; i < 100; ++i) {
         auto *data = new float[origin.total() * 3]();
         convert2float(origin, data);
         Image image(origin.rows, origin.cols, origin.channels(), data);
@@ -28,12 +28,50 @@ int main() {
         image.softmax();
 
 //展示结果
-//        image.show();
+        image.show();
+        auto end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        cout << "duration = " << duration << "ms,\n";
 //        cout << endl;
     }
-    auto end = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    cout << "duration = " << duration << "ms,\n";
+
+
+
+
+
+
+    origin = imread("../image/bg.jpg");
+    cout << "channels*rows*cols = " << origin.channels() << "*" << origin.rows << "*" << origin.cols << endl;
+
+
+    for (int i = 0; i < 10; ++i) {
+        cout << "bg & o3, ";
+        auto start = std::chrono::steady_clock::now();
+
+        auto *data = new float[origin.total() * 3]();
+        convert2float(origin, data);
+        Image image(origin.rows, origin.cols, origin.channels(), data);
+
+//3次卷积操作
+        for (int j = 0; j < 3; ++j) {
+            image.conv(conv_params[j].out_channels, conv_params[j].in_channels, conv_params[j].kernel_size,
+                       conv_params[j].pad, conv_params[j].stride, conv_params[j].p_weight, conv_params[j].p_bias);
+            image.reLU();
+            if (j < 2) image.max_pool(); //最后一次不需要池化
+        }
+
+//全连接
+        image.fc(fc_params->out_features, fc_params->p_weight, fc_params->p_bias);
+        image.softmax();
+
+//展示结果
+        image.show();
+        auto end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        cout << "duration = " << duration << "ms,\n";
+//        cout << endl;
+    }
+
 
 
 
@@ -41,7 +79,7 @@ int main() {
 
 
     cout << "(face & conv1) * 100, ";
-    start = std::chrono::steady_clock::now();
+    auto start = std::chrono::steady_clock::now();
 
     for (int i = 0; i < 100; ++i) {
         auto *data = new float[origin.total() * 3]();
@@ -66,8 +104,8 @@ int main() {
 //        image.show();
 //        cout << endl;
     }
-    end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     cout << "duration = " << duration << "ms,\n";
 
 
